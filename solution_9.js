916bffa0392294da8e25704c8e3b6a687ba38c97
David Hariri <david.hariri@gmail.com>
2016-01-09 11:57:15 -0500

    the solution for problem 9 is 31875000

---
+++ solution_9.js
@@ -0,0 +1,33 @@
+'use strict';
+console.time("s");
+
+/*
+NOTE:
+"Special Pythagorean triplet"
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 52.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+let s = 0;
+
+for (let c = 1; c < 1000; c++) {
+  for (let b = 1; b < 1000; b++) {
+    for (let a = 1; a < 1000; a++) {
+      if(a + b + c === 1000 && a < b < c) {
+        if(Math.pow(a, 2) + Math.pow(b, 2) === Math.pow(c, 2)) {
+          s = a*b*c;
+          break;
+        }
+      }
+    }
+  }
+}
+
+console.log(s);
+console.timeEnd("s");
